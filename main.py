aee570f49ad977a253c914448608a67af5e77040
sineokayaa <a.sineokaya@g.nsu.ru>
2023-03-02 11:15:04 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+Group:
+Sineokaya
+Varfolomeeva
+'''
+def triangle(x, y, a, b, angle, color):
+    '''
+    Function for drawing triangle
+    :param x:
+    :param y:
+    :param a:
+    :param b:
+    :param angle:
+    :param color:
+    :return: None
+    '''
+    pass
+
+def main():
+    '''
+    Maim function.
+    :return:
+    '''
